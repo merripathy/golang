5c0ef9a72d1845cda499a94d263f5c6c56ef67b3
merripathy <37133383+merripathy@users.noreply.github.com>
2020-04-11 15:35:11 +0530

    Create hellomod.go

---
+++ modules/hellomod/hellomod.go
@@ -0,0 +1,13 @@
+package hellomod
+
+import "fmt"
+
+//SayHello Module Export func
+func SayHello(){
+	fmt.Println("Hello, How are you")
+
+}
+//Hola Module Export func
+func Hola(){
+	fmt.Println("hola!! Como estas??")
+}
