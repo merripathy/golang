070b5b19d763fc8a6ddda1b629fd4b5fb98076e9
merripathy <37133383+merripathy@users.noreply.github.com>
2020-04-11 15:57:10 +0530

    Create secondtest.go
    second test

---
+++ module2/mymod/secondtest.go
@@ -0,0 +1,13 @@
+package hellomod
+
+import "fmt"
+
+//SayHello Module Export func
+func SayingHello(){
+	fmt.Println("Hello, How are you")
+
+}
+//Hola Module Export func
+func Holaing(){
+	fmt.Println("hola!! Como estas??")
+}
